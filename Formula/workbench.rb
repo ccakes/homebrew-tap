1264724fc5e72945fa1d3cbed085e1dbc154782d
ccakes <103831+ccakes@users.noreply.github.com>
2026-05-29 08:48:15 +0000

    chore: bump formulas to latest upstream releases

--- Formula/workbench.rb
+++ Formula/workbench.rb
@@ -1,28 +1,28 @@
 class Workbench < Formula
   desc "TUI process orchestrator for local development"
   homepage "https://github.com/ccakes/workbench"
-  version "0.6.4"
+  version "0.6.5"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ccakes/workbench/releases/download/v#{version}/bench-darwin-arm64"
-      sha256 "1c93001fcb1a263556d8a2130395edfc7ef501777e412e1ea6cf4a3b3836a839"
+      sha256 "90c10a5e71c1d486c200490d3bbd7e59459efccee9ce404949a5dde8d1d13255"
     end
     on_intel do
       url "https://github.com/ccakes/workbench/releases/download/v#{version}/bench-darwin-amd64"
-      sha256 "f510653897fb4df26901b22fa5501ccace8ed80a1ec995f635393e0d1216c409"
+      sha256 "09fa5f4d4d23771c31dd279ca61dddd47fb6b3cf4efedcc59314d8b4fe065525"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ccakes/workbench/releases/download/v#{version}/bench-linux-arm64"
-      sha256 "c40ce58c32acb74ff96b91fc0418960c7fbe7269943c09ae6560e9bfe86e7b7d"
+      sha256 "0b9f1350a16cb5dae5f212764d29313597b177780af8197cdb367a52d5998428"
     end
     on_intel do
       url "https://github.com/ccakes/workbench/releases/download/v#{version}/bench-linux-amd64"
-      sha256 "4cf98c86c4f6cc3a93561631861508ad82185e60bde5bfd440a8ddbc4fb12b1a"
+      sha256 "6a638899b44d750a0143f09c24336391feaa8f048a0ef26a0e8334987f9d6b1c"
     end
   end
 
