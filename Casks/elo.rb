7759fc2d2344e6bfda56b52d40f3ddb5a7043cf9
ccakes <103831+ccakes@users.noreply.github.com>
2026-06-20 07:37:15 +0000

    chore: bump formulas to latest upstream releases

--- Casks/elo.rb
+++ Casks/elo.rb
@@ -1,6 +1,6 @@
 cask "elo" do
-  version "0.3.0"
-  sha256 "a9a89bd63bb54263ce29c5ae0f2f614fd7fc79a0810c2c92280a485ee362172c"
+  version "0.3.1"
+  sha256 "bef66ecd39a9a3ce7f48249c658bb0de4db10cadfe1b3708c0645c65517c373e"
 
   url "https://github.com/ccakes/elo/releases/download/v#{version}/Elo-macos.app.zip"
   name "Elo"
